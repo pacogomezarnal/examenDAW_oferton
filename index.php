4c30da8e8e4078407bcf70ec8248e03ac2a45d75
Paco Gomez <fjgomez@florida-uni.es>
2017-05-10 23:55:01 +0200

    Examen Final oferton

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Club Catarroja</title>
+    <link rel="stylesheet" href="./css/estilos.css">
+  </head>
+  <body>
+    <!--MENU-->
+    <div class="menu">
+      <nav>
+        <ul>
+          <li><a href="index.html"><img src="./img/logo.png" alt=""></a></li>
+          <li><a href="#">Nuevo Oferton</a></li>
+          <li><a href="#">Listar Ofertones</a></li>
+        </ul>
+      </nav>
+    </div>
+    <!--MENU-->
+    <!--MENU-->
+    <div class="contenido">
+    </div>
+  </body>
+</html>
